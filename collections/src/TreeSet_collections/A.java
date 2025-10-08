ddea5e455731e82338ec3dbb2a0161d05ec81837
swamini10 <swaminibhandare10@gmail.com>
2025-10-08 11:07:13 +0530

    TreeSet

---
+++ collections/src/TreeSet_collections/A.java
@@ -0,0 +1,13 @@
+package TreeSet_collections;
+import java.util.TreeSet;
+
+public class A {
+	public static void main(String[] args) {
+		
+	TreeSet<String> names =new TreeSet<String>();
+names.add("swamini");
+names.add("soham");
+names.add("gayu");
+System.out.println(names);
+	}
+}
---
+++ collections/src/TreeSet_collections/Demo.java
@@ -0,0 +1,15 @@
+package TreeSet_collections;
+import java.util.TreeSet;
+public class Demo{
+			public static void main(String[] args) {
+				TreeSet<Integer> values =new TreeSet<Integer>();
+				values.add(30);
+				values.add(10);
+				values.add(20);
+				values.add(40);
+				values.add(35);
+				values.add(5);
+				System.out.println(values);
+			}
+}
+//
